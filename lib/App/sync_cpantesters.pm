fe8f10bc9568bef6f98a37c3832e5ed827c69ba6
Marcel Grünauer <marcel@cpan.org>
2007-11-23 14:09:27 +0000

    Import of MARCEL/App-sync_cpantesters-0.01 from CPAN.
    gitpan-cpan-distribution: App-sync_cpantesters
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCEL/App-sync_cpantesters-0.01.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

---
+++ lib/App/sync_cpantesters.pm
@@ -0,0 +1,71 @@
+package App::sync_cpantesters;
+
+use warnings;
+use strict;
+
+
+our $VERSION = '0.01';
+
+
+1;
+
+
+__END__
+
+
+
+=head1 NAME
+
+App::sync_cpantesters - Sync CPAN testers failure reports to local dir
+
+=head1 SYNOPSIS
+
+None.
+
+=head1 DESCRIPTION
+
+This is just a marker package so you can install this distribution from the
+CPAN shell.
+
+=head1 TAGS
+
+If you talk about this module in blogs, on del.icio.us or anywhere else,
+please use the C<appsync_cpantesters> tag.
+
+=head1 VERSION 
+                   
+This document describes version 0.01 of L<App::sync_cpantesters>.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<<bug-app-sync_cpantesters@rt.cpan.org>>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHOR
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 by Marcel GrE<uuml>nauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
+
+
