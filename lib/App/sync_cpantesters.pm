1ebb62e71ec18a3eb44609d9a37e7e107fd3059e
Marcel Grünauer <marcel@cpan.org>
2007-11-24 21:50:54 +0000

    Import of MARCEL/App-sync_cpantesters-0.04 from CPAN.
    gitpan-cpan-distribution: App-sync_cpantesters
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MARCEL/App-sync_cpantesters-0.04.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/App/sync_cpantesters.pm
+++ lib/App/sync_cpantesters.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 1;
@@ -34,7 +34,7 @@ please use the C<appsync_cpantesters> tag.
 
 =head1 VERSION 
                    
-This document describes version 0.03 of L<App::sync_cpantesters>.
+This document describes version 0.04 of L<App::sync_cpantesters>.
 
 =head1 BUGS AND LIMITATIONS
 
