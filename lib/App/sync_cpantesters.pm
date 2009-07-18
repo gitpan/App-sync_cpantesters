40de5f55f44eb014cbe71e16efbdfb5101d44302
Marcel Grünauer <marcel@cpan.org>
2009-07-18 18:40:19 +0000

    Import of MARCEL/App-sync_cpantesters-0.05 from CPAN.
    gitpan-cpan-distribution: App-sync_cpantesters
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         MARCEL/App-sync_cpantesters-0.05.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/App/sync_cpantesters.pm
+++ lib/App/sync_cpantesters.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 
 1;
@@ -16,32 +16,22 @@ __END__
 
 =head1 NAME
 
-App::sync_cpantesters - Sync CPAN testers failure reports to local dir
+App::sync_cpantesters - Sync CPAN testers failure reports to local directories
 
 =head1 SYNOPSIS
 
-None.
+    # sync_cpantesters -a MARCEL -d ~/dev/cpan-testers
 
 =head1 DESCRIPTION
 
 This is just a marker package so you can install this distribution from the
 CPAN shell.
 
-=head1 TAGS
-
-If you talk about this module in blogs, on del.icio.us or anywhere else,
-please use the C<appsync_cpantesters> tag.
-
-=head1 VERSION 
-                   
-This document describes version 0.04 of L<App::sync_cpantesters>.
-
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
-Please report any bugs or feature requests to
-C<<bug-app-sync_cpantesters@rt.cpan.org>>, or through the web interface at
+Please report any bugs or feature requests through the web interface at
 L<http://rt.cpan.org>.
 
 =head1 INSTALLATION
@@ -54,13 +44,13 @@ The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
 site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Marcel GrE<uuml>nauer
+Copyright 2007-2008 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
